e44fbc75df005fd7709d8bb7c55bb904472fe4c5
Ilke Dincer <ilke.dincer@hotmail.com>
2019-11-16 23:32:43 +0800

    camera identifies faces and direction to turn

--- find_face_wave.py
+++ find_face_wave.py
@@ -13,23 +13,48 @@ import cv2
 import numpy as np
 
 ################################################# Functions
-TOLERANCE = 10 # in pixels
+TOLERANCE = 100 # in pixels
 
 
-def turn():
-    pass
+def find_location(screen_middle, item_x, frane):
+    if item_x < screen_middle - TOLERANCE:
+        turn_left(frane)
+    elif screen_middle + TOLERANCE < item_x:
+        turn_right(frane)
+    else:
+        in_middle(frane)
+
+def turn_right(frane):
+    h, w = frane.shape[:2]
+    y = int(h / 2)
+    x = int(w - 10)
+    cv2.circle(frane, (x, y), 8, (0, 0, 255), -1)
+
+
+def turn_left(frane):
+    h, w = frane.shape[:2]
+    y = int(h / 2)
+    cv2.circle(frane, (10, y), 8, (0, 0, 255), -1)
+
+
+def in_middle(frane):
+    h, w = frane.shape[:2]
+    y = int(h / 2)
+    x = int(w / 2)
+    cv2.circle(frane, (x, y), 8, (0, 0, 255), -1)
 
 
 # https://pythonprogramming.net/loading-video-python-opencv-tutorial/?completed=/loading-images-python-opencv-tutorial/
 
 ################################################# Begin Video Capture
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) # begins camera at port 0
+
 # ----- writing video to file
 # fourcc = cv2.VideoWriter_fourcc(*"XVID")
 # out = cv2.VideoWriter("output.avi", fourcc, 20, (640, 480))
 # cap.set(15, 0.1)
 
-x_centre = cap.get(3) / 2
+x_centre = cap.get(3) / 2 # gets video width and divides by 2
 
 isOpen = True
 
@@ -41,6 +66,7 @@ while (isOpen):
         isOpen = False
     
     ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # out.write(frame) # writes to file
     faceCascade = cv2.CascadeClassifier("C:\\Users\\Ilke\\AppData\\Local\\Programs\\Python\\Python37-32\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")
@@ -52,12 +78,15 @@ while (isOpen):
     
     print("Found {0} faces".format(len(faces)))
 
-
+    face_mid_x = 0
     for (x, y, w, h) in faces:
         # cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        face_mid_x = int(x + w / 2)
 
-    # if ((x_centre - ()))
+    # if ((x_centre - ()))\
+    if face_mid_x != 0:
+        find_location(x_centre, face_mid_x, frame)
     
     cv2.imshow('frame', frame)
 
---
+++ poppy_movements.py
@@ -0,0 +1,23 @@
+#
+# poppy_movements.py - file for testing vision
+# Project - makerspace Poppy robot
+#
+# Author: Ilke Dincer
+# Revisions:        initial version
+#
+
+from pypot.creatures import PoppyHumanoid
+
+poppy = PoppyHumanoid()
+
+[p.name for p in poppy.proimitives]
+
+def wave():
+    pass
+
+def turn_left():
+    # poppy.head_z.goal_position = 5
+    pass
+
+def turn_right():
+    pass
